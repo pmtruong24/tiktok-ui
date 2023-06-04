8310c2707ef62ad7dea1db5c160666327fbf4813
phamminhtruong <pmtruong2472@gmail.com>
2023-06-04 16:04:12 +0700

    Configure Route and Layout.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Fragment } from 'react';
+import { publicRoutes } from '~/routes';
+import DefaultLayout from '~/components/Layouts/DefaultLayout';
+
 function App() {
     return (
-        <div className="App">
-            <h2>React App</h2>
-        </div>
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        const Page = route.component;
+
+                        let Layout = DefaultLayout;
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
--- src/components/Button/index.js
+++
@@ -1,7 +0,0 @@
-import myAlert from "~/components/Content/index";
-
-function Button() {
-    return <button onClick={myAlert}>Click</button>
-}
-
-export default Button;
